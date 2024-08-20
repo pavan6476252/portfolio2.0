29455affe6a3f48879a226ebc03135c377b0db5f
the-gitter <meesalapavan2001@gmail.com>
2024-08-20 15:06:10 +0530

    fix : home

--- apps/website/src/home/components/hero-section-social-icon.component.tsx
+++ apps/website/src/home/components/hero-section-social-icon.component.tsx
@@ -4,6 +4,7 @@ import EditSocialPlatformsComponent from "./social-platforms.edit";
 import { useAppSelector } from "../../store/store";
 import EditContext from "../../context/edit-context";
 import { TbEditCircle } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 function HeroSectionSocialIconsComponent() {
   const resumeState = useAppSelector((state) => state.home);
@@ -22,7 +23,11 @@ function HeroSectionSocialIconsComponent() {
       />
       {resumeState.resume.socialPlatforms.map((platform) => {
         const Icon = getIconByName(platform.name);
-        return <Icon />;
+        return (
+          <Link to={platform.link} target="_blank">
+            <Icon />
+          </Link>
+        );
       })}
       {editMode && (
         <TbEditCircle
